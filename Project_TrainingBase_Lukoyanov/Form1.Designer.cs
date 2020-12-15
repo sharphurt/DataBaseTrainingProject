8f2f773faee804e392c494e592bbfc210a7dbc32
sharphurt <#ugs;37cxoi[97cpnx908z209>
2020-12-15 22:34:05 +0500

    Main button form

--- Project_TrainingBase_Lukoyanov/Form1.Designer.cs
+++ Project_TrainingBase_Lukoyanov/Form1.Designer.cs
@@ -29,13 +29,106 @@ namespace Project_TrainingBase_Lukoyanov
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+			this.label1.Location = new System.Drawing.Point(150, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(264, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Учебная база данных";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(155, 54);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(258, 48);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Продавцы";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button2.Location = new System.Drawing.Point(155, 120);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(258, 48);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Покупатели";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button3.Location = new System.Drawing.Point(155, 184);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(258, 47);
+			this.button3.TabIndex = 3;
+			this.button3.Text = "Заказы";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button4.Location = new System.Drawing.Point(155, 245);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(257, 47);
+			this.button4.TabIndex = 4;
+			this.button4.Text = "Детали заказа";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button5.Location = new System.Drawing.Point(154, 308);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(258, 47);
+			this.button5.TabIndex = 5;
+			this.button5.Text = "Продукты";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(551, 384);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Учебная база";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
 	}
 }
 
--- Project_TrainingBase_Lukoyanov/Form1.cs
+++ Project_TrainingBase_Lukoyanov/Form1.cs
@@ -16,5 +16,10 @@ namespace Project_TrainingBase_Lukoyanov
 		{
 			InitializeComponent();
 		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
